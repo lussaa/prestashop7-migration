821140d3fe18db2406c6db4e87e44eaab25537e6
Seb E <seb@nospam.org>
2021-02-12 13:52:34 +0100

    Cosmetic

--- www-scripts/import-categories.php
+++ www-scripts/import-categories.php
@@ -16,7 +16,7 @@ if ($res) {
 const EN = 1;
 const FR = 2;
 
-
+// Recreate category 1/Home. Only 0/Root was kept after deleting "all"
 $home = new Category();
 $home->force_id = true;
 $home->id_category = 2;
@@ -30,20 +30,20 @@ $home->add();
 
 echo "Importing\n";
 
-$datas = array();
-while (($data = fgetcsv(STDIN, 1000, ";")) !== FALSE) {
-    $datas[] = $data;
+$raw_categories = array();
+while (($raw_category = fgetcsv(STDIN, 1000, ";")) !== FALSE) {
+    $raw_categories[] = $raw_category;
 }
 
 
 echo "Downloading images\n";
 
-foreach($datas as $data) {
-  $id = $data[0];
+foreach($raw_categories as $raw_category) {
+  $id = $raw_category[0];
   $legacy_id = ((int) $id) - 1;
   $image_url = "https://www.stickaz.com/img/c/" . $legacy_id .".jpg";
   $image_path = "./img/c/" . $id . ".jpg";
-  $image = file_get_contents($image_url);
+  $image = @file_get_contents($image_url);
   if ($image !== FALSE) {
     file_put_contents($image_path, $image, LOCK_EX);
   }
@@ -67,21 +67,21 @@ $ic->regenerateThumbnails();
 
 
 echo "Creating categories\n";
-foreach($datas as $data) {
-  $id = $data[0];
+foreach($raw_categories as $raw_category) {
+  $id = $raw_category[0];
   $c = new Category($id);
   $c->force_id = true;
   $c->id_category = $id;
   $c->id = $id;
   $c->id_image = (int) $id;
-  $c->id_parent = (int) $data[1];
-  $c->name = [ EN => $data[2], FR => $data[6] ];
-  $c->description = [ EN => $data[3], FR => $data[7] ];
-  $c->link_rewrite = [ EN => $data[4], FR => $data[8] ];
-  $c->meta_keywords = [ EN => $data[5], FR => $data[9] ];
+  $c->id_parent = (int) $raw_category[1];
+  $c->name = [ EN => $raw_category[2], FR => $raw_category[6] ];
+  $c->description = [ EN => $raw_category[3], FR => $raw_category[7] ];
+  $c->link_rewrite = [ EN => $raw_category[4], FR => $raw_category[8] ];
+  $c->meta_keywords = [ EN => $raw_category[5], FR => $raw_category[9] ];
   $c->add();
 
-  echo "Inserted category: " . $c->id . " - " . $data[2] . " (parent " . $data[1] . ")\n"; 
+  echo "Inserted category: " . $c->id . " - " . $raw_category[2] . " (parent " . $raw_category[1] . ")\n"; 
 }
 
 echo "Done\n";
